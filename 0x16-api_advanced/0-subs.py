9d7f876236afef40b5537595a196daf04b92f687
Soly-M-27 <solymar227@icloud.com>
2022-06-01 17:44:04 +0000

    Fixed import issues with costime user

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -9,7 +9,7 @@ import requests
 def number_of_subscribers(subreddit):
     ''' Returns total number of subs '''
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    headers = {'User-Agent': 'User Agent'}
+    headers = {'User-Agent': 'Its a me'}
     response = requests.get(url, headers=headers, allow_redirects=False)
 
     if response.status_code != 200:
