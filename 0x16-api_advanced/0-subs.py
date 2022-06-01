fea139d04c91b2a918264787f8465fd161cf6ed3
Soly-M-27 <solymar227@icloud.com>
2022-06-01 17:27:02 +0000

    Get total numbers of subs from Reddit API

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+''' function that queries the Reddit API and returns the number
+of subscribers (not active users, total subscribers) for a given
+subreddit '''
+
+import request
+
+
+def number_of_subscribers(subreddit):
+    ''' Returns total number of subs '''
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    headers = {'User-Agent': "It's a me"}
+    response = request.get(url, header=headers, allow_redirects=False)
+
+    if response.status_code != 200:
+        return 0
+    else:
+        return response.json().get('data').get('subscribers')
