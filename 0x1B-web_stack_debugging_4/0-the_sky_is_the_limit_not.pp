0df284fc4b58d7285cf719d969d4590c2a51c62a
Soly-M-27 <solymar227@icloud.com>
2022-06-09 18:48:59 +0000

    fixed syntax error

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -2,6 +2,6 @@
 # within the traffic
 
 exec {'modify max open files limit setting':
-command => 'sed -i "s/15/10000" /etc/default/nginx && sudo srvice nginx restart',
+command => 'sed -i "s/15/10000" /etc/default/nginx && sudo service nginx restart',
 path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
 }
